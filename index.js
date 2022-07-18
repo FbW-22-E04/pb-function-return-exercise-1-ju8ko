8c3a429f9a259d94b018717cd65eed1264c24790
ju8ko <julia.x.kobzar@gmail.com>
2022-07-18 15:31:21 +0200

    Add index.js

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 //Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
 // 1
 function uppercase(str) {
-  //write your code here 
-}
+  const words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
 
+  return words.join(" ");
+}
 console.log(uppercase("the quick brown fox"));
 
 // 2
 // Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
 function find_longest_word(str) {
-  //write your code here 
+  //write your code here
+  const words = str.split(" ");
+  let longestWord = "";
+  for (i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
 }
 console.log(find_longest_word("Web Development Tutorial"));
 
 // 3
 // Write a JavaScript function that accept a list of country names as input and returns the longest country name as output.
 function Longest_Country_Name(country_name) {
-  //write your code here 
+  //write your code here
+  let longestCountry = "";
+  for (i = 0; i < country_name.length; i++) {
+    if (country_name[i].length > longestCountry.length) {
+      longestCountry = country_name[i];
+    }
+  }
+  return longestCountry;
 }
 
 console.log(
